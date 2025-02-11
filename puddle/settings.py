a80d049d2a7cb00bd46b599525b3a46f8dbe52de
fazeel <fazeel@gmail.com>
2025-02-11 20:25:01 +0500

    tailwindcss locally installed

--- puddle/settings.py
+++ puddle/settings.py
@@ -46,7 +46,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+NPM_BIN_PATH = r"C:\\Program Files\\nodejs\\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'puddle.urls'
--- puddle/urls.py
+++ puddle/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('item/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('inbox/',include('conversation.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
